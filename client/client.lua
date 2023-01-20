d789607a1db210a5f62dadcd05eb85124dec1ce6
DESKTOP-638T0UE\PUTTER <saranpat.vong@gmail.com>
2023-01-21 00:53:06 +0700

    fix(client): text entry bug

--- client/client.lua
+++ client/client.lua
@@ -1,29 +1,24 @@
+CreateThread(function()
 
+  for k, v in ipairs(Config.blips) do
 
-Citizen.CreateThread(function()
-
-  for k,v in ipairs(Config.Blip) do 
-
-      local blip = AddBlipForRadius(v.x, v.y, v.z , v.radius or 0) -- you can use a higher number for a bigger zone
-
-	    SetBlipHighDetail(blip, true)
-	    SetBlipColour(blip, v.radiusColor or 15)
-	    SetBlipAlpha (blip, 128)
-
-      local blip = AddBlipForCoord(v.x, v.y, v.z)
-      SetBlipSprite (blip, v.id)
-      SetBlipDisplay(blip, v.display)
-      SetBlipScale(blip, v.size) 
-      SetBlipColour(blip, v.color)
-      SetBlipAsShortRange(blip, true)
-      SetBlipBright(blip, true)
-      SetBlipHighDetail(blip, true)
-      BeginTextCommandSetBlipName("STRING")
-      AddTextComponentString(v.text)
-      EndTextCommandSetBlipName(blip)
+    if v.radius and v.radiusColor then
+      local radiusBlip = AddBlipForRadius(v.coords.x, v.coords.y, v.coords.z, v.radius or 0) -- you can use a higher number for a bigger zone
+      SetBlipHighDetail(radiusBlip, true)
+      SetBlipColour(radiusBlip, v.radiusColor or 15)
+      SetBlipAlpha(radiusBlip, 128)
+    end
+    local blip = AddBlipForCoord(v.coords.x, v.coords.y, v.coords.z)
+    SetBlipSprite(blip, v.id)
+    SetBlipDisplay(blip, v.display)
+    SetBlipScale(blip, v.scale)
+    SetBlipColour(blip, v.color)
+    SetBlipAsShortRange(blip, true)
+    SetBlipHighDetail(blip, true)
+    local blipName = 'extend_blipcustom'
+    AddTextEntry(blipName, v.text)
+    BeginTextCommandSetBlipName(blipName)
+    EndTextCommandSetBlipName(blip)
   end
 
 end)
-
-
-
