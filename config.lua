f811958645145ea52fc8aec42d322ba072f6cf28
DESKTOP-638T0UE\PUTTER <saranpat.vong@gmail.com>
2023-01-21 00:53:38 +0700

    refactor(config): change x,y,z tp vec3 coords

--- config.lua
+++ config.lua
@@ -1,174 +1,140 @@
-Config = {}
+Config = {
+    blips = {
+        {
+            coords = vec3(4889.5, -4924.88, 3.38),
+            id = 93,
+            color = 43,
+            scale = 0.6,
+            display = 4,
+            text = '🍹 Beach Bar ~p~[Bar]',
+        },
+
+        {
+            coords = vec3(360.47, 279.1, 91.19),
+            id = 93,
+            color = 48,
+            scale = 0.6,
+            display = 4,
+            text = '🍾 GalaxyPub ~r~[Pub]'
+        },
 
 
-Config.Blip = {
-    {
-        x = 4889.5 ,
-        y = -4924.88 ,
-        z = 3.38 , 
-        size = 0.6 ,
-        id = 93 ,
-        color = 43 ,
-        display = 4 , 
-        text = '🍹 Beach Bar ~p~[Bar]'
-    },
+        -------------- Garage ------------------------
 
-    {
-        x = 360.47 ,
-        y = 279.1 ,
-        z = 91.19 , 
-        size = 0.6 ,
-        id = 93 ,
-        color = 48 ,
-        display = 4 , 
-        text = '🍾 GalaxyPub ~r~[Pub]'
-    },
+        {
+            coords = vec3(-319.38, -887.42, 31.07),
+            id = 50,
+            color = 0,
+            scale = 0.9,
+            display = 4,
+            text = '<font face="chakra">การาจกลาง</font>'
+        },
+        {
+            coords = vec3(-801.76, -1296.05, 5.0),
+            id = 50,
+            color = 3,
+            scale = 0.9,
+            display = 4,
+            text = '<font face="chakra">~b~การาจท่าเรือ</font>'
+        },
+        {
+            coords = vec3(-561.88, 309.85, 84.97),
+            id = 50,
+            color = 70,
+            scale = 0.9,
+            display = 4,
+            text = '<font face="chakra">~y~การาจเหลือง</font>'
+        },
+        {
+            coords = vec3(1107.96, 2666.2, 84.97),
+            id = 50,
+            color = 1,
+            scale = 0.9,
+            display = 4,
+            text = '<font face="chakra">~r~การาจแดง</font>'
+        },
+        {
+            coords = vec3(905.06, -49.4, 31.85),
+            id = 50,
+            color = 8,
+            scale = 0.9,
+            display = 4,
+            text = '<font face="chakra">~q~การาจคาสิโน</font>'
+        },
 
+        ------------------------------------
 
-    -------------- Garage ------------------------
+        {
+            coords = vec3(1691.1, 3251.53, 39.75),
+            id = 84,
+            color = 1,
+            scale = 0.6,
+            display = 4,
+            radius = 220.0,
+            radiusColor = 1,
+            text = '<font face="chakra">~w~#เรเบล</font>'
+        },
 
-    {
-        x = -319.38 ,
-        y = -887.42 ,
-        z = 31.07 , 
-        size = 0.9 ,
-        id = 50 ,
-        color = 0 ,
-        display = 4 , 
-        text = '<font face="chakra">{~w~[การาจกลาง]</font>'
-    },
-    {
-        x = -801.76 ,
-        y = -1296.05 ,
-        z = 5.0 , 
-        size = 0.9 ,
-        id = 50 ,
-        color = 3 ,
-        display = 4 , 
-        text = '<font face="chakra">{~b~[การาจท่าเรือ]</font>'
-    },
-    {
-        x = -561.88 ,
-        y = 309.85 ,
-        z = 84.97 , 
-        size = 0.9 ,
-        id = 50 ,
-        color = 70 ,
-        display = 4 , 
-        text = '<font face="chakra">{~y~[การาจเหลือง]</font>'
-    },
-    {
-        x = 1107.96 ,
-        y = 2666.2 ,
-        z = 84.97 , 
-        size = 0.9 ,
-        id = 50 ,
-        color = 1 ,
-        display = 4 , 
-        text = '<font face="chakra">{~w~เบิกเท่านั้น~r~[การาจแดง]</font>'
-    },
-    {
-        x = 905.06 ,
-        y = -49.4 ,
-        z = 31.85 , 
-        size = 0.9 ,
-        id = 50 ,
-        color = 8 ,
-        display = 4 , 
-        text = '<font face="chakra">{~p~[การาจคาสิโน]</font>'
-    },
-   
-    ------------------------------------
+        {
+            coords = vec3(-1338.5, -1278.48, 37.42),
+            id = 362,
+            color = 2,
+            scale = 0.7,
+            display = 4,
+            text = '<font face="chakra">~w~หน้ากาก ~b~[ร้านค้า]</font>'
+        },
 
+        {
+            coords = vec3(236.46, 222.55, 106.29),
+            id = 309,
+            color = 69,
+            scale = 1.0,
+            display = 4,
+            text = '<font face="chakra">~g~[ธนาคาร]</font>'
+        },
 
-    {
-        x = 1691.1 ,
-        y = 3251.53 ,
-        z = 39.75 , 
-        size = 0.6 ,
-        id = 84 ,
-        color = 1 ,
-        display = 4 , 
-        radius = 220.0,
-        radiusColor = 1,
-        text = '<font face="chakra">~w~#เรเบล</font>'
-    },
+        {
+            coords = vec3(2747.29, 3473.07, 54.04),
+            id = 308,
+            color = 0,
+            scale = 1.3,
+            display = 2,
+            text = '<font face="chakra">~p~[ร้านขายอุปกรณ์]</font>'
+        },
 
-    -----------------------------------
+        {
+            coords = vec3(1860.84, 3695.69, 34.28),
+            scale = 0.8,
+            id = 60,
+            color = 57,
+            display = 2,
+            text = '<font face="chakra">~w~#สถานีตำรวจ ~r~[ทะเลทราย]</font>'
+        },
 
-    {
-        x = -1338.5 ,
-        y = -1278.48 ,
-        z = 37.42 ,
-        size = 0.7 ,
-        id = 362 ,
-        color = 2 ,
-        display = 4 , 
-        text = '<font face="chakra">~w~หน้ากาก ~b~[ร้านค้า]</font>'
-    },
+        {
+            coords = vec3(1817.46, 3670.26, 34.28),
+            scale = 0.8,
+            id = 61,
+            color = 52,
+            display = 2,
+            text = '<font face="chakra">~w~#โรงพยาบาล ~r~[ทะเลทราย]</font>'
+        },
+        {
+            coords = vec3(-426.75, 1132.47, 2.56),
+            scale = 1.6,
+            id = 401,
+            color = 47,
+            display = 2,
+            text = '<font face="chakra">~w~#พักผ่อน ~r~[AFK]</font>'
+        },
 
-    {
-        x = 236.46 ,
-        y = 222.55 ,
-        z = 106.29 ,
-        size = 1.0 ,
-        id = 309 ,
-        color = 69 ,
-        display = 4 , 
-        text = '<font face="chakra">~g~[ธนาคาร]</font>'
-    },
-    
-    {
-        x = 2747.29 ,
-        y = 3473.07 ,
-        z = 54.04 , 
-        size = 1.3 ,
-        id = 308 ,
-        color = 0 ,
-        display = 2 , 
-        text = '<font face="chakra">~p~[ร้านขายอุปกรณ์]</font>'
-    },
-
-    {
-        x = 1860.84 ,
-        y = 3695.69 ,
-        z = 34.28 , 
-        size = 0.8 ,
-        id = 60 ,
-        color = 57 ,
-        display = 2 ,
-        text = '<font face="chakra">~w~#สถานีตำรวจ ~r~[ทะเลทราย]</font>'
-    },
-
-    {
-        x = 1817.46 ,
-        y = 3670.26 ,
-        z = 34.28 , 
-        size = 0.8 ,
-        id = 61 ,
-        color = 52 ,
-        display = 2 ,
-        text = '<font face="chakra">~w~#โรงพยาบาล ~r~[ทะเลทราย]</font>'
-    },
-    {
-        x = -426.75 ,
-        y = 1132.47 ,
-        z = 2.56 , 
-        size = 1.6 ,
-        id = 401 ,
-        color = 47 ,
-        display = 2 ,
-        text = '<font face="chakra">~w~#พักผ่อน ~r~[AFK]</font>'
-    },
-
-    {
-        x = -1279.84 ,
-        y = -303.25 ,
-        z = 31.85 , 
-        size = 0.9 ,
-        id = 59 ,
-        color = 48 ,
-        display = 4 , 
-        text = '<font face="chakra">{~p~[ร้านเกม]</font>'
-    },
+        {
+            coords = vec3(-1279.84, -303.25, 31.85),
+            scale = 0.9,
+            id = 59,
+            color = 48,
+            display = 4,
+            text = '<font face="chakra">{~p~[ร้านเกม]</font>'
+        },
+    }
 }
