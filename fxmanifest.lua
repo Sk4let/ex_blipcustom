588238980e926e2ed3eb2a1e75a6f2962f011853
DESKTOP-638T0UE\PUTTER <saranpat.vong@gmail.com>
2023-01-21 00:53:19 +0700

    chore: bump manifest to v0.0.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,19 @@
---[[ FX Information ]]--
-fx_version   'cerulean'
-lua54        'yes'
-game         'gta5'
+--[[ FX Information ]] --
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+game 'gta5'
 
---[[ Resource Information ]]--
-name         'extend_blipcustom'
-author       'http://extend.studio/'
-version      '0.0.1'
-description  ''
+--[[ Resource Information ]] --
+name 'extend_blipcustom'
+version '0.0.2'
+description 'extend blipcustom'
+author 'http://extend.studio/'
+repository 'https://github.com/Sk4let/extend_blipcustom'
 
---[[ Manifest ]]--
+--[[ Manifest ]] --
 
 client_scripts {
-	'config.lua',
-    "client/client.lua",
+    'config.lua',
+    'client/client.lua',
 }
-
-
