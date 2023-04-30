fb1c04865f2246c0ed35c6c908c39b93fb561766
PARK\saran <saranpat.vong@gmail.com>
2023-04-30 08:22:37 +0700

    chore: bump manifest version to v0.0.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ game 'gta5'
 
 --[[ Resource Information ]] --
 name 'extend_blipcustom'
-version '0.0.2'
+version '0.0.3'
 description 'extend blipcustom'
 author 'http://extend.studio/'
 repository 'https://github.com/Sk4let/extend_blipcustom'
